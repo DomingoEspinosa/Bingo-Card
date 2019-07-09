e86c2cbce4e266d1e93dd2f40e78881019656635
Domingo Espinosa <domingo.espinosa@codurance.com>
2019-07-09 12:38:10 +0200

    Refactor

--- src/main/java/BingoCard.java
+++ src/main/java/BingoCard.java
@@ -1,24 +1,20 @@
 import java.util.*;
 
 public class BingoCard {
-    public String[] getCard() {
+    public  String[] getCard() {
         LinkedHashSet<String> generated = new LinkedHashSet<>();
-        while (generated.size() < 5){
-            generated.add(getRandomNumberInRange(1, 15, "B"));
-        }
-        while (generated.size() < 10){
-            generated.add(getRandomNumberInRange(16, 30, "I"));
-        }
-        while (generated.size() < 14){
-            generated.add(getRandomNumberInRange(31, 45, "N"));
-        }
-        while (generated.size() < 19){
-            generated.add(getRandomNumberInRange(46, 60, "G"));
-        }
-        while (generated.size() < 24){
-            generated.add(getRandomNumberInRange(61, 75, "O"));
-        }
+        generatedElement(generated, 5, 1, 15, "B");
+        generatedElement(generated, 10, 16, 30, "I");
+        generatedElement(generated, 14, 31, 45, "N");
+        generatedElement(generated, 19, 46, 60, "G");
+        generatedElement(generated, 24, 61, 75, "O");
         return generated.stream().toArray(value -> new String[value]);
+    }
+
+    private void generatedElement(LinkedHashSet<String> generated, int size, int min, int max, String letter) {
+        while (generated.size() < size) {
+            generated.add(getRandomNumberInRange(min, max, letter));
+        }
     }
 
     private String getRandomNumberInRange(int min, int max, String letter) {
